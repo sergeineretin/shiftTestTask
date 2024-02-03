abe1667e20fb67d4547b0b139a3468a7c97beb14
Sergei Neretin <sergeizzzneretin@gmail.com>
2024-02-04 00:05:02 +0700

    fix: private fields in the Options class now match the template for naming constants

--- src/main/java/com/sergeineretin/shift/Options.java
+++ src/main/java/com/sergeineretin/shift/Options.java
@@ -1,31 +1,31 @@
 package com.sergeineretin.shift;
 
 public class Options {
-    private static final String statisticsFull = "-f";
-    private static final String statisticsSimple = "-s";
-    private static final String pathOfResults = "-o";
-    private static final String prefix = "-p";
+    private static final String STATISTICS_FULL = "-f";
+    private static final String STATISTICS_SIMPLE = "-s";
+    private static final String PATH_OF_RESULTS = "-o";
+    private static final String PREFIX = "-p";
 
     public static boolean isOption(final String string) {
-        return string.equals(statisticsFull)
-                || string.equals(statisticsSimple)
-                || string.equals(pathOfResults)
-                || string.equals(prefix);
+        return string.equals(STATISTICS_FULL)
+                || string.equals(STATISTICS_SIMPLE)
+                || string.equals(PATH_OF_RESULTS)
+                || string.equals(PREFIX);
     }
 
     public static String getStatisticsFull() {
-        return statisticsFull;
+        return STATISTICS_FULL;
     }
 
     public static String getStatisticsSimple() {
-        return statisticsSimple;
+        return STATISTICS_SIMPLE;
     }
 
     public static String getPrefix() {
-        return prefix;
+        return PREFIX;
     }
 
     public static String getPathOfResults() {
-        return pathOfResults;
+        return PATH_OF_RESULTS;
     }
 }
